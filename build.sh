1af7a79ba846ab126ec35367ad25d001be856cfa
Sabbir Ahmed <sabbiirr@gmail.com>
2017-01-15 22:14:26 +0600

    Added build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+srcdir=`pwd`
+_target=x64
+
+cd "${srcdir}"/electron
+LDFLAGS="${LDFLAGS} -Wl,-z,noexecstack"
+script/bootstrap.py --verbose \
+                  --target_arch=${_target} \
+                  --clang_dir=/usr \
+                  --build_libchromiumcontent \
+                  --defines="${_chromium_flags[*]}"
+script/build.py -c Release
